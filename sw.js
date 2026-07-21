884f73f5457e5735c9e0697b98b3ce9a90ceb8ed
naparstekd-afk <naparstekd@gmail.com>
2026-07-21 14:02:22 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'lichka-v5';
+const CACHE = 'lichka-v6';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e=>{
